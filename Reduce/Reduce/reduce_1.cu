5c294fecb6f73a6e0b4abcc04d04ed2ff2764b17
Jon2222 <595912199@qq.com>
2026-07-31 11:53:12 +0800

    reduce子项目新增准备数据、启动配置、设备内存

--- Reduce/Reduce/reduce_1.cu
+++ Reduce/Reduce/reduce_1.cu
@@ -11,10 +11,44 @@ do {                                                     \
 	}                                                    \
 	}while(0)
 
+#define BLOCK_SIZE 256
 
 
 int main()
 {
+	// 准备数据
+	const int N = 1 << 24;
+	const size_t bytes = N * sizeof(int);
 
+	int* h_in = (int*)malloc(bytes);
+	int cpuSum = 0;
+	for (int i = 0;i < N;i++)
+	{
+		h_in[i] = 1;
+		cpuSum += h_in[i];
+	}
+
+	// 启动配置
+	dim3 block(BLOCK_SIZE);
+	dim3 grid((N + block.x - 1) / block.x);
+
+	//设备内存
+	int* d_in, * d_partial;
+	CUDA_CHECK(cudaMalloc(&d_in, bytes));
+	CUDA_CHECK(cudaMalloc(&d_partial, grid.x * sizeof(int)));
+	CUDA_CHECK(cudaMemcpy(d_in, h_in, bytes, cudaMemcpyHostToDevice));
+
+	// 多趟归约
+
+	// warm up
+
+	// 计时
+
+	// 结果
+
+	// 清理
+	cudaFree(d_in);
+	cudaFree(d_partial);
+	free(h_in);
 	return 0;
 }
