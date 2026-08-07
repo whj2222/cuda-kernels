f1a3c42280a3b9c24311f1ce02b367e6e99b8b6d
Jon2222 <595912199@qq.com>
2026-08-07 14:31:21 +0800

    cProfile基础用法

--- Profile/01_basic_run.py
+++ Profile/01_basic_run.py
@@ -1,7 +1,15 @@
 import cProfile
 from workload import slow_sum, call_heavy_sum
 def main():
+    print("slow_sum : ")
     cProfile.run("slow_sum(2_000_000)")
 
+    print("call_heavy_sum : ")
+    cProfile.run("call_heavy_sum(2_000_000)")
+
+    n = 1000000
+    cProfile.runctx("slow_sum(n)", globals(), locals(), sort="cumulative")
+
+    cProfile.run("slow_sum(1000000)", filename="01_basic.prof")
 if __name__ == '__main__':
     main()
